5b4d4715ec191ae0de71ccbba42f0d06ee56e240
SorrowCrow <78910309+SorrowCrow@users.noreply.github.com>
2021-10-14 01:57:40 +0300

    header/menu

---
+++ client/src/components/Menu.tsx
@@ -0,0 +1,66 @@
+import { FC, useEffect, useState } from "react";
+import { ReactComponent as Logo } from "../assets/logo.svg";
+import { ReactComponent as MenuSvg } from "../assets/menu.svg";
+import { ReactComponent as Shop } from "../assets/shop.svg";
+import { useDeckContext } from "./DeckContext";
+
+const Menu: FC = () => {
+    const deckId = useDeckContext().deckId;
+    const decks = useDeckContext().decks;
+
+    const [lastScrollTop, setlastScrollTop] = useState(0);
+
+    useEffect(() => {
+        window.addEventListener(
+            "scroll",
+            function () {
+                var st = window.pageYOffset || document.documentElement.scrollTop;
+                const element = document.getElementById("sub-menu");
+
+                if (st > lastScrollTop && element !== null && st !== null) {
+                    console.log("yeeet");
+                    element.style.bottom = "100%";
+                } else if (element !== null && st !== null) {
+                    element.style.bottom = "0";
+                }
+                setlastScrollTop(st);
+            },
+            false
+        );
+    }, []);
+
+    function menuHover(e: React.MouseEvent<SVGSVGElement, MouseEvent>) {
+        // e.currentTarget.style.background = "white";
+        // e.currentTarget.style.fill = "purple";
+    }
+    function menuOut(e: React.MouseEvent<SVGSVGElement, MouseEvent>) {
+        // e.currentTarget.style.background = "";
+        // e.currentTarget.style.fill = "";
+    }
+
+    return (
+        <header className={`header fixed overflow-hidden`}>
+            <div className={`flex content-between main-menu`}>
+                <div className={`flex menu`}>
+                    <MenuSvg className="menuSvg h-p" onMouseLeave={menuOut} onMouseEnter={menuHover} fill={`white`} />
+                    <div className={`align-self-center`}>PLAYING ARTS</div>
+                </div>
+                <Logo fill="white" className={`align-self-center`} />
+                <Shop className={`h-p shop`} fill={`purple`} />
+            </div>
+            <div className={`flex content-center sub-menu relative`} id={`sub-menu`}>
+                <div className={`items flex`}>
+                    <div className={`item h-p`}>ZERO</div>
+                    <div className={`item h-p`}>ONE</div>
+                    <div className={`item h-p`}>TWO</div>
+                    <div className={`item h-p`}>THREE</div>
+                    <div className={`item h-p`}>SPECIAL</div>
+                    <div className={`item h-p`}>FUTURE</div>
+                    <div className={`item h-p ${decks[0].name === "Crypto Edition" ? "activated" : ""}`}>CRYPTO</div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Menu;
---
+++ client/src/components/headerComponents/DeckHeader.tsx
@@ -0,0 +1,68 @@
+import { FC, useEffect, useState } from "react";
+import { useDeckContext } from "../DeckContext";
+import { ReactComponent as OpenseaButton } from "../../assets/openseaBtn.svg";
+import { ReactComponent as Share } from "../../assets/share.svg";
+import DeckMenu from "../deckComponents/DeckMenu";
+
+const DeckHeader: FC = () => {
+    const decks = useDeckContext().decks;
+    const [width, setWindowWidth] = useState(0);
+
+    useEffect(() => {
+        updateDimensions();
+
+        window.addEventListener("resize", updateDimensions);
+        return () => window.removeEventListener("resize", updateDimensions);
+    }, []);
+
+    const updateDimensions = () => {
+        const width = window.innerWidth;
+        setWindowWidth(width);
+    };
+    const items = [];
+    for (let i = 0; i < width / 326; i++) {
+        items.push(<div className={`looped-item`}>11 days / 11 cards of giveaways</div>);
+    }
+
+    const content = <div className={`flex mg--3 looped align-center`}>{items}</div>;
+
+    return (
+        <>
+            <div className={`DeckHeader`}>
+                <div className={`mgl-2`}>
+                    <h1 className={`flex`}>
+                        {decks[0].name}
+                        {decks[0].name === "Crypto Edition" && <div className={`nft`}>NFT</div>}
+                    </h1>
+                    <div className={`description`}>
+                        A deck of playing cards featuring works of 55 leading artists.
+                        <br /> Unique digital art collectibles living on the Ethereum blockchain.
+                    </div>
+                    <DeckMenu
+                        items={[
+                            <>
+                                <OpenseaButton className={`openseaBtn h-p`} />
+                                <Share className={`h-p`} />
+                            </>,
+                            "CARDS",
+                            "SUPPLY",
+                            "STATS",
+                            "PRODUCT",
+                            "GALLERY",
+                            "ROADMAP",
+                        ]}
+                    />
+                </div>
+            </div>
+            <div className={`bg-daysleftCarousel`}>
+                <div className={`daysleftCarousel flex mg-1 overflow-hidden relative align-center`}>
+                    <div className={`fader`}></div>
+                    {content}
+                    <div className={`fader tr-180`}></div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default DeckHeader;
